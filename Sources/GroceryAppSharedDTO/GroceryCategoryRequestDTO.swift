7878cdd487ac4c3f59cb671d7d5cb238006570a0
Rumen Guin <rumen2k@gmail.com>
2023-05-25 12:51:12 +0530

    added grocery category request DTO and response DTO

---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryRequestDTO.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by RUMEN GUIN on 25/05/23.
+//
+
+import Foundation
+
+public struct GroceryCategoryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by RUMEN GUIN on 25/05/23.
+//
+
+import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+}
