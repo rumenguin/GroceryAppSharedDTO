36ee048b6c56214a52a644ef79f39700d69b8877
Rumen Guin <rumen2k@gmail.com>
2023-05-26 20:45:23 +0530

    add DTOs for grocery item

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by RUMEN GUIN on 26/05/23.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+    
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(title: String, price: Double, quantity: Int) {
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by RUMEN GUIN on 26/05/23.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO: Codable {
+    
+    public let id: UUID
+    public let title: String
+    public let price: Double
+    public let quantity: Int
+    
+    public init(id: UUID, title: String, price: Double, quantity: Int) {
+        self.id = id
+        self.title = title
+        self.price = price
+        self.quantity = quantity
+    }
+    
+}
